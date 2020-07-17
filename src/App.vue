ab236f5fc8edde7b6c8d839524411ae23ad58ed4
wkm-wangZhe <wangzhe01@sinoiov.com>
2020-07-17 13:27:00 +0800

    33232

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
+<!-- @format -->
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello world
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  mounted() {
+    console.log(233423423);
+  },
+  components: {}
+};
 </script>
 
 <style>
