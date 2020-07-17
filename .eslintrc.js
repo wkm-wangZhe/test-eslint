f55f31e24dd388a030bdc13f9d1a6ea2c8a2041e
wkm-wangZhe <wangzhe01@sinoiov.com>
2020-07-17 14:10:00 +0800

    更新 eslint prettier pre-commit 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,40 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-      es6: true
-    },
-    rules: {
-      'no-console': 2,
-      'no-useless-escape': 0,
-      'no-multiple-empty-lines': [
-        2,
-        {
-          max: 3
-        }
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          semi: true,
-          trailingComma: 'none',
-          bracketSpacing: true,
-          jsxBracketSameLine: true,
-          insertPragma: true,
-          requirePragma: false
-        }
-      ]
-    },
-    plugins: [],
-    extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'eslint:recommended'],
-    parserOptions: {
-      parser: 'babel-eslint'
-    }
-  }
-  
-  // "husky": {
-  //     "hooks": {
-  //       "pre-commit": "yarn run precommit-msg && lint-staged"
-  //     }
-  //   },
-  
+	root: true,
+	env: {
+		node: true,
+		es6: true,
+	},
+	rules: {
+		"no-console": 0,
+		"no-useless-escape": 0,
+		"no-multiple-empty-lines": [
+			2,
+			{
+				max: 3,
+			},
+		],
+		"prettier/prettier": [
+			"error",
+			{
+				endOfLine: "crlf",
+				printWidth: 200,
+				tabWidth: 2,
+				useTabs: true,
+				singleQuote: false,
+				bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
+				jsxBracketSameLine: false, //  在jsx中把'>' 是否单独放一行
+			},
+		],
+	},
+	plugins: [],
+	extends: ["plugin:vue/essential", "plugin:prettier/recommended", "eslint:recommended"],
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+};
+
+// "husky": {
+//     "hooks": {
+//       "pre-commit": "yarn run precommit-msg && lint-staged"
+//     }
+//   },
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
-<!-- @format -->
-
 <template>
-  <div id="app">
-    hello world
-  </div>
+	<div id="app">
+		hello world
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  mounted() {
-    console.log(233423423);
-  },
-  components: {}
+	name: "App",
+	mounted() {
+		console.log(233423423);
+	},
+	components: {},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
